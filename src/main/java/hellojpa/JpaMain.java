8bbf1b99aa4fa2e47120dbdbc7852e5acf3980d8
BJW <backj123@fgi.kr>
2023-06-28 21:16:30 +0900

    [BJW] 2023/06/28 회사에서 JAP공부.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,23 +11,22 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager manager =  emf.createEntityManager();
+        EntityManager em =  emf.createEntityManager();
+        EntityTransaction tx =  em.getTransaction();
 
-        EntityTransaction tx =  manager.getTransaction();
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("진욱2");          manager.persist(member);
-            Member findmember = manager.find(Member.class, 1L);
-            findmember.setName("HelloJPA");
+            Member member = em.find(Member.class, 100L);
 
+            System.out.println(member);
+
+            System.out.println("====================");
             tx.commit();
         }catch (Exception e){
             tx.rollback();
         }finally {
-            manager.close();
+            em.close();
         }
         emf.close();
     }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,13 +4,25 @@ import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR")
 @Data
 public class Member {
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
+    private int age;
+    public Member(){  }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
